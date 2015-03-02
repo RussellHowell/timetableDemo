f55aef11608ed5ee35191e4a5c1ea8dc209f83af
Russell Howell <rhowell1110@gmail.com>
2015-03-01 20:39:32 -0500

    updated Timetable.java

--- Timetable.java
+++ Timetable.java
@@ -4,22 +4,14 @@
 import java.util.Scanner;
 import java.util.regex.*;
 import javafx.util.converter.TimeStringConverter;
+import java.text.DecimalFormat;
+
+
 
 public class Timetable {
 
 	public static void main(String[] args) {
 		
-		int[] southCampus = {10,30,50,110,130,810,830,850,910,930,950,1010,1030,1050,1110,1130,1150,
-				1230,1250,1310,1330,1350,1410,1430,1450,1510,1530,1550,1610,1630,1650,
-				1710,1730,1750,1810,1830,1850,1910,1930,1950,2010,2030,2050,
-				2110,2130,2150,2210,2230,2250,2310,2330,2350};		
-		int[] northIthan = {806,826,835,846,855,906,915,926,935,946,955,1006,1015,1026,
-				1035,1046,1055,1106,1115,1126,1135,1146,1155,1235,1235,1246,1255,1306,
-				1315,1326,1335,1346,1355,1406,1415,1426,1435,1446,1455,1506,1515,1526,1535,1546,
-				1555,1606,1615,1626,1635,1646,1655,1706,1715,1726,1735,1746,1755,1806,1815,1826,1835,1846,
-				1855,1906,1915,1926,1935,1946,1955,2006,2015,2026,2035,2046,};
-		
-	
 		
 		Scanner scan = new Scanner(System.in);
 		
@@ -27,13 +19,13 @@ public class Timetable {
 							+ "\n3) Falvey Library" + "\n4) Tolintine Hall" + "\n5) Garey Hall/Law School" 
 							+ "\n6) West Campus");
 		
-		int start_loc= scan.nextInt(); //Start Location Value
+		int start_loc= scan.nextInt(); //Start Location Value (1-6)
 		
 		System.out.println("Choose destination:"+"\n1) South Campus" + "\n2) North Ithan Gate"
 				+ "\n3) Falvey Library" + "\n4) Tolintine Hall" + "\n5) Garey Hall/Law School" 
 				+ "\n6) West Campus");
 		
-		int end_loc = scan.nextInt(); //End Location Value
+		int end_loc = scan.nextInt(); //End Location Value (1-6)
 		
 		System.out.println("Enter the time you want to arrive before in the following format: hh:mm");
 		
@@ -45,19 +37,121 @@ public class Timetable {
 		
 		//get returns a four digit integer in 24hr  format.
 		int time_int = formatTime(time_string, meridiem);
+		shuttleTimes shuttle_info = new shuttleTimes();
 		
+		int tripArray[] = {0,0}; //Create array to hold dep. (index 0) and arrival (index 1)
+								// times to be passed by reference
+								//(**Java Hack, fix later)
+		
+		findTrip(start_loc,end_loc,time_int, shuttle_info, tripArray);
+		
+		formatOutput(tripArray);
+		System.out.println(tripArray[0]);
 	}
 	
 	
+
+	private static void formatOutput(int[] tripArray) {
+		
+		
+
+	}
+
+
+
+	private static void findTrip(int start_loc, int end_loc, int time_int, shuttleTimes shuttle_info, int tripArray[]) {
+		
+		//This switch case finds the arrival time based on 
+		//the "arrive by" and destination parameters set by the user
+		switch(end_loc){
+			
+			case 1:
+				tripArray[1] = findInArray(time_int, shuttle_info.southCampus);
+				break;
+				
+			case 2:
+				tripArray[1] = findInArray(time_int, shuttle_info.northIthan);
+				break;
+		
+			case 3:
+				tripArray[1] = findInArray(time_int, shuttle_info.falveyLibrary);
+				break;
+			
+			case 4:
+				tripArray[1] = findInArray(time_int, shuttle_info.tolentineHall);
+				break;
+				
+			case 5:
+				tripArray[1] = findInArray(time_int, shuttle_info.lawSchool);
+				break;
+				
+			case 6:
+				tripArray[1] = findInArray(time_int, shuttle_info.westCampus);
+				break;		
+		}
+		
+		//This switch case finds the departure time based on 
+		//the arrival time and departure location parameter set by the user
+		switch(end_loc){
+					
+			case 1:
+				tripArray[0] = findInArray(tripArray[1], shuttle_info.southCampus);
+				break;
+			case 2:
+				tripArray[0] = findInArray(tripArray[1], shuttle_info.northIthan);
+				break;
+			case 3:
+				tripArray[0] = findInArray(tripArray[1], shuttle_info.falveyLibrary);
+				break;
+			case 4:
+				tripArray[0] = findInArray(tripArray[1], shuttle_info.tolentineHall);
+				break;
+			case 5:
+				tripArray[0] = findInArray(tripArray[1], shuttle_info.lawSchool);
+				break;
+			case 6:
+				tripArray[0] = findInArray(tripArray[1], shuttle_info.westCampus);
+				break;
+				
+				}
+	}
+
+
+	//this method find the closest lower value in the passed array
+	private static int findInArray(int time_int, int[] timeArray) {
+		int i = 0;
+		int result=0;
+		
+		//Increment index
+		outerloop:
+		while(i<= timeArray.length){
+			
+			if(timeArray[i]>time_int){
+				result = timeArray[i-1];
+				break outerloop;
+			}
+			else if (timeArray[i]==time_int){
+				result = timeArray[i];
+				break outerloop;
+			}//end of if - else
+				
+			++i;
+		}//end of while loop
+		
+		return result;
+	}
+
+
 
 	private static int formatTime(String time_string, int meridiem) {
 		
 		//Remove colon from string convert to integer
 		int arrive_before = Integer.parseInt(time_string.replaceAll("[\\D]",""));
-		//Convert back to string
-		time_string = Integer.toString(arrive_before);
 		
-		
+		//Convert back to string and keep formatting (leading 0)
+		DecimalFormat IntForm = new DecimalFormat("0000");
+		time_string = IntForm.format(arrive_before); 
+	
 		//Check if the entered time is of the right format
 		if(time_string.length()<4||time_string.length()>5){ //length
 			System.out.println("ERROR: Make sure you enter the time in the format hh:mm");
@@ -80,11 +174,11 @@ public class Timetable {
 				
 			arrive_before=Integer.parseInt(time_string);
 				
-			}else{} 
+			} 
 		
 		return arrive_before;
-	}//End of method
 	
+	}//End of method
 	
 }
 
